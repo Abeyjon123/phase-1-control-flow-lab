ccff99388eb7a24fce2e1aed070d3112eda3aa51
jonathanrobertabeyta <entitypbc@gmail.com>
2021-10-11 21:23:37 -0600

    commit tree 1d1048d0feb3f5da1914123b5e39f1ce415f53cd

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceTraveled){
+  if (distanceTraveled <= 400)
+    return ('This one is on me!');
+  else if (distanceTraveled > 2500)
+    return ('No can do.');
+  else if (distanceTraveled >= 2000)
+   return ('I will gladly take your thirty bucks.');
+  }
+
+function ternaryCheckCity(destination){
+  const properDestination = "NYC";
+    return((destination == properDestination) ? 'Ok, sounds good.' : 'No go.');
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipGenerosity){
+  switch (tipGenerosity) {
+    case 'generous':
+      return('Thank you so much.');
+      break;
+    case 'not as generous':
+      return('Thank you.')
+      break;
+    case 'thanks for everything':
+      return('Bye.');
+  }
 }
